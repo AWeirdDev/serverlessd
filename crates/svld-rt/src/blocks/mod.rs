00d66094cce81bf4cbe7bca33485dcbc9921935e
AWeirdDev <aweirdscratcher@gmail.com>
2026-05-19 14:17:10 +0800

    refactor: name to `http_client` instead of `client`

--- crates/svld-rt/src/blocks/mod.rs
+++ crates/svld-rt/src/blocks/mod.rs
@@ -1,9 +1,9 @@
 //! Extensible blocks attached to the worker state.
 
-mod client;
 mod core;
+mod http_client;
 mod replier;
 
-pub use client::HttpClientBlock;
 pub use core::{Block, Blocks};
+pub use http_client::HttpClientBlock;
 pub use replier::{MaybeReplier, Replier, ReplierBlock, Reply};
