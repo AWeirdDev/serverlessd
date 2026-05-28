da1bb9a2d3542f82e570de22d7ab25f43d60c921
AWeirdDev <aweirdscratcher@gmail.com>
2026-05-28 22:55:10 +0800

    refactor: accept kebab-case instead of snake_case

--- crates/svld-configs/src/serverlessd.rs
+++ crates/svld-configs/src/serverlessd.rs
@@ -1,6 +1,7 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "kebab-case")]
 pub struct ServerlessdConfig {
     /// The port to bind the HTTP server to.
     #[serde(default = "defaults::port")]
